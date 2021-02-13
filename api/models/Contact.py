042c88f109e5c131d82f68a821c967181a2952de
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2021-02-13 09:52:36 -0300

    Documentação do modelo Contact

--- api/models/Contact.py
+++ api/models/Contact.py
@@ -25,6 +25,22 @@ class Contact(Model):
     email = TextField()
 
     def to_dict(self):
+        """Convert an Contact objecto to a dictionary.
+
+        The dict contains the following structure:
+        {
+            "id": self.id,
+            "name": self.name,
+            "photo_url": self.photo_url,
+            "email": self.email,
+        }
+
+        Returns
+        -------
+        Dict
+            Dictionary containing all the informations about a Contact
+
+        """
         return {
             "id": self.id,
             "name": self.name,
@@ -34,6 +50,22 @@ class Contact(Model):
 
     @staticmethod
     def dict_to_object(data):
+        """Convert a given dictionary to a Contact Object.
+
+        The dict must contains the following structure:
+        {
+            "id": self.id,
+            "name": self.name,
+            "photo_url": self.photo_url,
+            "email": self.email,
+        }
+
+        Parameters
+        ----------
+        data : Contact
+            Contact object to converto a dict structure
+
+        """
         return Contact(
             id=data["id"],
             name=data["name"],
@@ -43,7 +75,27 @@ class Contact(Model):
 
     @staticmethod
     def multiples_json_contacts_to_objects(json):
+        """Convert the request from google people api into a useful list.
 
+        This methods create a list containing all the contacts got from api,
+        storing only id, name, photo_url and email.
+
+        Parameters
+        ----------
+        json : dict
+            dictionary returned from Google People Api, connections GET.
+
+        Returns
+        -------
+        List
+            a list containing contacts objects, in the following way:
+            [
+                Contact(id="123", name="test", photo_url="...", email="@..."),
+                Contact(id="345", name="name", photo_url="...", email="@..."),
+                Contact(id="456", name="name_t", photo_url="...", email="@...")
+            ]
+
+        """
         connections = json["connections"]
 
         data = {"contacts": []}
@@ -62,12 +114,40 @@ class Contact(Model):
 
     @staticmethod
     def _get_domain(data):
-        print(data["email"].split("@"))
+        """Get the domain.
+
+        The domain are the right side after @, in other words:
+        email => "teste@abcde.com" means
+        domain => "@abcde.com"
+
+        Parameters
+        ----------
+        data : Union[Dict, Contact]
+            Contact specific data, given in a dict format ou Contact object
+
+        Returns
+        -------
+        str
+            String containing the domain
+
+        """
         return data["email"].split("@")[1]
 
     @staticmethod
     def get_unique_domains(data):
+        """Get a set of unique domains in a list of contacts.
 
+        Parameters
+        ----------
+        data : List
+            A list with multiples contacts
+
+        Returns
+        -------
+        set
+            A set composed by unique domains found in data
+
+        """
         domains = set()
 
         for connection in data:
@@ -77,6 +157,26 @@ class Contact(Model):
 
     @staticmethod
     def group_by_email_group(data):
+        """Get a grouped by domains list of contacts.
+
+        This methods creates an dictionary with key as unique domain and pass
+        a list of contacts with the same domain.
+
+        Parameters
+        ----------
+        data : List
+            List of contacts
+
+        Returns
+        -------
+        dict
+            Dictionary following this structure:
+            {
+                'domain1': [contact1, contact2],
+                'domain2': [contact3, contact3]
+            }
+
+        """
         data = data["contacts"]
 
         domains = Contact.get_unique_domains(data)
